8e96404d2a14dde42b7f320baffde6a9a50a5934
[hannahtong7] <[s2743332@ed.ac.uk]>
2025-04-08 15:18:20 +0100

    Final commit pt 2.

---
+++ MetaDigitise/MetaDigitise_figure_extraction.R
@@ -0,0 +1,111 @@
+install.packages("metaDigitise")
+library(metaDigitise)
+setwd("~/Documents/temp/meta/MetaDigitise/Figures/Paper1")
+data <- metaDigitise(dir = "~/Documents/temp/meta/MetaDigitise/Figures/Paper1")
+#type 1
+#type s
+print(data)
+
+
+
+data <- metaDigitise(dir = "~/Documents/temp/meta/MetaDigitise/Figures/Paper11.1")
+#type 1
+#type s
+print(data)
+
+data <- metaDigitise(dir = "~/Documents/temp/meta/MetaDigitise/Figures/Paper11.2")
+#type 1
+#type s
+print(data)
+
+data <- metaDigitise(dir = "~/Documents/temp/meta/MetaDigitise/Figures/Paper12")
+#type 1
+#type s
+print(data)
+
+data <- metaDigitise(dir = "~/Documents/temp/meta/MetaDigitise/Figures/Paper15.1")
+#type 1
+#type s
+print(data)
+
+data <- metaDigitise(dir = "~/Documents/temp/meta/MetaDigitise/Figures/Paper15.2")
+#type 1
+#type s
+print(data)
+
+data <- metaDigitise(dir = "~/Documents/temp/meta/MetaDigitise/Figures/Paper16.1")
+#type 1
+#type s
+print(data)
+
+data <- metaDigitise(dir = "~/Documents/temp/meta/MetaDigitise/Figures/Paper16.2")
+#type 1
+#type s
+print(data)
+
+data <- metaDigitise(dir = "~/Documents/temp/meta/MetaDigitise/Figures/Paper17.11")
+#type 1
+#type s
+print(data)
+
+data <- metaDigitise(dir = "~/Documents/temp/meta/MetaDigitise/Figures/Paper20")
+#type 1
+#type s
+print(data)
+
+data <- metaDigitise(dir = "~/Documents/temp/meta/MetaDigitise/Figures/Paper21.1")
+#type 1
+#type s
+print(data)
+
+data <- metaDigitise(dir = "~/Documents/temp/meta/MetaDigitise/Figures/Paper21.2")
+#type 1
+#type s
+print(data)
+
+data <- metaDigitise(dir = "~/Documents/temp/meta/MetaDigitise/Figures/Paper23")
+#type 1
+#type s
+print(data)
+
+data <- metaDigitise(dir = "~/Documents/temp/meta/MetaDigitise/Figures/Paper25")
+#type 1
+#type s
+print(data)
+
+data <- metaDigitise(dir = "~/Documents/temp/meta/MetaDigitise/Figures/Paper28.1")
+#type 1
+#type s
+print(data)
+
+
+data <- metaDigitise(dir = "~/Documents/temp/meta/MetaDigitise/Figures/Paper28.2")
+#type 1
+#type s
+print(data)
+
+data <- metaDigitise(dir = "~/Documents/temp/meta/MetaDigitise/Figures/Paper28.3")
+#type 1
+#type s
+print(data)
+
+
+data <- metaDigitise(dir = "~/Documents/temp/meta/MetaDigitise/Figures/Paper34.1")
+print(data)
+
+data <- metaDigitise(dir = "~/Documents/temp/meta/MetaDigitise/Figures/Paper34.2")
+print(data)
+
+data <- metaDigitise(dir = "~/Documents/temp/meta/MetaDigitise/Figures/Paper35")
+print(data)
+
+data <- metaDigitise(dir = "~/Documents/temp/meta/MetaDigitise/Figures/Paper36")
+print(data)
+
+data <- metaDigitise(dir = "~/Documents/temp/meta/MetaDigitise/Figures/Paper40")
+print(data)
+
+data <- metaDigitise(dir = "~/Documents/temp/meta/MetaDigitise/Figures/Paper44")
+print(data)
+
+
